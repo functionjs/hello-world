f0b2373cce3dfc4dd1319c68028cd7185bbb3f13
Arthur <38524934+functionjs@users.noreply.github.com>
2025-05-07 23:27:54 +0300

    Update usacoOM1.cpp by 3 steps

--- httphelloworld/usacoOM1.cpp
+++ httphelloworld/usacoOM1.cpp
@@ -1,4 +1,4 @@
-//USACO Ooh Milk! ( may 03/0/2025)
+//USACO Ooh Milk! ( may 7 2025) by three steps!
 // N D
 // A B
 // m1 ... mN
@@ -40,7 +40,7 @@ int A, B;   // A is Jhon active bottles, B is Nohj one
 
 vector <long> Milk; //[MaxN];  // Milks in N bottles
 Map RLE;  // Run Length Encoding  to get rid of total sorting
-MapIt it, it_next; // smart pointers to element of RLE
+MapIt it, it_next, it_cut; // smart pointers to element of RLE
 
 int a=A-B; //a - active bottles with smallest number of milk drips 
 int base=0;  // base height for left part of stairs
@@ -57,66 +57,77 @@ long result = 0;  // result
           } 
      }
 
-     void floodBasin(MapIt it, long flood_volume){// it - first step of Basin, flood_volume of milk 
-         cout << "floodBasin:  flood_volume = " << flood_volume << endl;    
-           if(flood_volume==0) return; 
+     void changeKeyRLE(long cur_level, long new_level  ){
+                                                         auto element = RLE.extract(cur_level);
+                                                          element.key() = new_level;
+                                                           RLE.insert(move(element));
+     }                                                   
 
-            long  pos_of_bottles=0;
-            long  pos_of_bottles_of_prev_step=0 ; //by default; 
+     MapIt floodBasin(MapIt it, long flood_volume){// it - first step of Basin, flood_volume of milk 
+         cout << "floodBasin:  flood_volume = " << flood_volume << endl;    
+           if(flood_volume==0) return it; 
+
+            long pos_of_bottles = 0;
+            long pos_of_bottles_of_prev_step = 0 ; //by default; 
             long stepW = 0; 
             long stepH = MaxMilk;//by default;
-            long cur_level=0;
-            long next_level=MaxMilk; //by default;
-            MapIt it_next;
-            long v= 0;
+            long cur_level = 0;
+            long next_level = MaxMilk; //by default;
+            long v = 0;  // volume of current step    
              do{
-                pos_of_bottles = it->second ; 
                 pos_of_bottles_of_prev_step=0 ;//by default; 
-                if(pos_of_bottles>0) 
-                  pos_of_bottles_of_prev_step = prev(it)->second ; 
-                 
-                 stepW =  pos_of_bottles - pos_of_bottles_of_prev_step;    
-   
+                pos_of_bottles = it->second ; 
+                 if(pos_of_bottles>0) 
+                   pos_of_bottles_of_prev_step = prev(it)->second ; 
+                  
+                  stepW =  pos_of_bottles - pos_of_bottles_of_prev_step;    
+    
                 stepH = MaxMilk; //by default
                 cur_level = it->first;
                 next_level = MaxMilk; //by default;
                 it_next = next(it); //next step pointer 
                  if(it_next != end(RLE)){ //if next exists
-                    next_level = it_next->first;
-                     stepH = next_level - cur_level; //hight of current step; 
+                     next_level = it_next->first;
+                      stepH = next_level - cur_level; //hight of current step; 
                  }    
                   v= stepW*stepH;     // volume of current step    
-               
-                  cout << "floodBasin: stepW = " << stepW << " stepH = " << stepH << " v = " << v << endl;   
-   
+                   cout << "floodBasin: stepW = " << stepW << " stepH = " << stepH << " v = " << v << endl;   
+    
                    if(flood_volume<v){
-                      long delta_level      = flood_volume/stepW;
-                      long partial_on_level = flood_volume%stepW;  
-                       long new_level = cur_level + delta_level;
-                        if(delta_level>0){
-                                          auto element = RLE.extract(cur_level);
-                                           element.key() = new_level;
-                                            RLE.insert(move(element));
-                        }
-                         if(partial_on_level>0) {
-                             RLE[new_level] =  stepW - partial_on_level;                    
-                             RLE[new_level+1] = partial_on_level;                 
+//                     cout << "   floodBasin: if(flood_volume<v)"<< endl;
+                       long delta_level      = flood_volume/stepW;
+//                       cout << "   floodBasin: delta_level ="<<delta_level <<endl;
+                       long partial_on_level = flood_volume%stepW;  
+//                       cout << "   floodBasin: partial_on_level="<<partial_on_level <<endl;
+                        long new_level = cur_level + delta_level;
+//                        cout << "   floodBasin: new_level="<<new_level <<endl;
+                         if(delta_level>0){
+                                           changeKeyRLE(cur_level, new_level  );  
+                                          // auto element = RLE.extract(cur_level);
+                                          //       element.key() = new_level;
+                                          //        RLE.insert(move(element));
+                                                   it = RLE.find(new_level);
+//                            cout << "   floodBasin: delta_level>0 it: "<<it->first <<"->"<< it->second<<endl;                                                   
                          }
-                         return;
+                          if(partial_on_level>0) {
+                              RLE[new_level] -=  partial_on_level;                    
+                               RLE[new_level+1] = RLE[new_level] + partial_on_level;                 
+//                              cout << "   floodBasin: partial_on_level>0 RLE[new_level]="<<RLE[new_level] <<" RLE[new_level+1] ="<< RLE[new_level+1]<<endl;                                                   
+                          }
+                           return it;
                    }
-                   // else
-                      // common case
-                      RLE.erase(cur_level);
-                      flood_volume -=v;
-                      it = it_next; 
+                   // else common case (flood_volume >= v)
+                    flood_volume -= v;   // correct remaining flood_volume by volume v  
+                    cout<< " ... floodBasin: flood_volume="<<flood_volume<<endl;
+                    it = it_next;       //  now we go to next step
+                     RLE.erase(cur_level); //fully flooded current step with volume v does not exists
                 } while(flood_volume>0);  
              
-                  return;  
-
+                  return it;  
      }
 
 int main(){
-    cout << "Ooh Milk to be optimized!"<<endl;
+    cout << "Ooh Milk by 3 steps!"<<endl;
     cin >> N >> D;
     cin >> A >> B;
     cout << "N="<<N<<" D="<<D<<" A="<<A<< " B="<<B<<endl;
@@ -136,7 +147,7 @@ int main(){
 //     cout << " nonTouchedBottles="<< nonTouchedBottles <<endl;      
        for(int i=0; i<nonTouchedBottles; i++)result += Milk[i]*Milk[i];
  
-    //Make RLE from M[] from  nonTouchedBottles to N
+    //Make RLE from M[] from  nonTouchedBottles to N-1
     int  sumb = 0; //sum of bottles
         RLE[0L]=0;
          it = RLE.begin();
@@ -144,13 +155,13 @@ int main(){
                 long m=Milk[i]; //m is current key (milks quantity in bottle)
                 int c=1;    //c is value (count of bottles with that (m) quantity of milk) 
                  i+=1;
-                  while((i<N) && (Milk[i]==m)){//while Milk[j]==m we count bottles
+                  while((i<N) && (Milk[i]==m)){//while Milk[i]==m we count bottles
                          c++; 
                          i+=1;
                   }
                    sumb += c; 
                     RLE[m]=sumb; 
-                     if(a>=sumb){
+                     if(sumb<=a){
                         base_width=sumb;
                         ++it;
                      }
@@ -159,14 +170,33 @@ int main(){
             cout << "base = "<< base << " base_width="<<base_width<<endl;                 
             cout << " active noncutted RLE element: "<<it->first << "->" <<it->second << endl;
             
-            //1) first flooding righ basin by volume1=yellowWitdh*Days
+            //1) first: flooding right basin by flood_volume=yellowWitdh*Days
              long yellowWidth = a - it->second;
-              long volume1=yellowWidth*D;
-               cout << "yellowWidth="<<yellowWidth<<" volume1="<<volume1<<endl;
-                floodBasin(next(it), volume1);
+              long flood_volume = yellowWidth*D;
+               cout << "yellowWidth="<<yellowWidth<<" volume1="<<flood_volume<<endl;
+               it_cut = floodBasin(next(it), flood_volume);
                 printRLE();
-            //2) lifing left part and  citting horizontally and calulating pooped volume 
-            //3) second flooding of modified right basin 
+                long h_cut = it_cut->first; 
+                 cout << " it_cut: " << h_cut << "->"<< it_cut->second << endl; 
+            //2) lifing left part and  cutting horizontally and then calulating pooped volume 
+             cout << " active noncutted RLE element afte flooding: "<<it->first << "->" <<it->second << endl;     
+            long v_milk_tsunami = 0;
+             for(auto i=it; i != RLE.begin(); ){
+                   long h= i->first;
+                    long h_new = h + D;  
+                     long diff =  h_new - h_cut; 
+                    cout << "h="<<  h <<" diff="<< diff <<" "<<i->first  <<  "->" << i->second << endl;
+                       if(diff<0)break;
+                        v_milk_tsunami += diff;
+                         --i;
+                          RLE.erase(h);
+             }
+            cout << "v_milk_tsunami = "<< v_milk_tsunami<< endl;
+            printRLE();
+            //3) second (tsunami) flooding of modified right basin 
+              floodBasin(it_cut, v_milk_tsunami);
+              cout<<" after tsunami:" << endl;
+               printRLE(); 
         /*    
            // Simulating   of game by increasing days by h (to increase efficiency)
            cout << "Simulating of "<<D<<" days game by increasing days by ? (to increase efficiency):"<<endl;
@@ -211,8 +241,12 @@ int main(){
                d +=h ; //? 
             }                   
         */
-        
-          for(auto& e : RLE) result +=  e.first*e.first * e.second;
+          
+          for(it= RLE.begin(), it_next=next(it); it_next!=RLE.end(); it=it_next, it_next=next(it)){
+             long q = it_next->second - it->second;
+             long m = it_next->first;
+              result +=  m*m * q;
+          }   
               cout << result%MOD<< endl;
 
             return 0;
@@ -256,9 +290,33 @@ We must add to a=A-B=4-2=2 bottles by one drip in.
 
 
 
+0 step 
+0             [0] 
+1********     [8]
+2**********   [10]
+3**********   [10]
+4**********   [10]
+
+1 step 
+0                   [0] 
+1******************        [18]
+2*************     [13]
+3*************     [13]
+4**************    [14]
+
+2 step 
+0                   [0] 
+1*************|*****        [18]
+2*************|     [13]
+3*************|     [13]
+4**************     [14]
+
+3 step 
+0                   [0] 
+1**************     [14]
+2**************     [14]
+3***************     [15]
+4***************     [15]
 
 
-
-
-  
 */ 
